01d962100f80f5ad6c10992110fbbc53fd11322d
Ell Neal <elliot@emdentec.com>
2018-12-27 18:17:07 +0000

    Add Result source

--- Sources/Result/Result.swift
+++ Sources/Result/Result.swift
@@ -1,3 +1,45 @@
-struct Result {
-    var text = "Hello, World!"
+public enum Result<Value, Error: Swift.Error> {
+    case value(Value)
+    case error(Error)
 }
+
+extension Result {
+    @discardableResult public func resolve() throws -> Value {
+        switch self {
+        case .value(let value):
+            return value
+        case .error(let error):
+            throw error
+        }
+    }
+}
+
+extension Result: Equatable where Value: Equatable, Error: Equatable { }
+extension Result: Hashable where Value: Hashable, Error: Hashable { }
+
+extension Result: CustomStringConvertible where Value: CustomStringConvertible, Error: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .value(let value):
+            return value.description
+        case .error(let error):
+            return error.description
+        }
+    }
+}
+
+extension Result: CustomDebugStringConvertible where Value: CustomDebugStringConvertible,
+Error: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        switch self {
+        case .value(let value):
+            return value.debugDescription
+        case .error(let error):
+            return error.debugDescription
+        }
+    }
+}
+
+extension Never: Error { }
+public typealias InfallibleResult<Value> = Result<Value, Never>
+public typealias LemonResult<Error: Swift.Error> = Result<Never, Error>
