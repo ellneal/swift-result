2c7571f598b59bbc4b6aee21345d4d29374974e4
Ell Neal <elliot@emdentec.com>
2019-01-25 12:36:50 +0000

    Rename EmptyResult.value --> EmptyResult.success (makes more semantic sense)

--- Sources/Result/EmptyResult.swift
+++ Sources/Result/EmptyResult.swift
@@ -1,7 +1,12 @@
 public typealias EmptyResult<Error: Swift.Error> = Result<Void, Error>
 
 extension Result where Value == Void {
+    @available(*, deprecated, renamed: "success")
     public static var value: Result {
+        return .success
+    }
+
+    public static var success: Result {
         return .value(())
     }
 }
